cc5c4eb80b41b36eeced90bfc0e8d80fb6e88d67
dariel312 <darielbarroso1998@gmail.com>
2018-07-31 16:03:34 -0400

    Added Snu Markdown decoder

--- ReReddit/wwwroot/app/common/htmldecode.filter.js
+++ ReReddit/wwwroot/app/common/htmldecode.filter.js
@@ -1,7 +1,10 @@
 ﻿const HtmlDecodeFilter = function () {
     return function (html) {
 
-        var decoded = angular.element('<textarea />').html(html).text();
+        //var decoded = angular.element('<textarea />').html(html).text();
+
+        var decoded = SnuOwnd.getParser().render(html);
+
 
         return decoded;
     };
--- ReReddit/wwwroot/js/app.js
+++ ReReddit/wwwroot/js/app.js
@@ -225,7 +225,10 @@ const DefaultIconComponent = {
 const HtmlDecodeFilter = function () {
     return function (html) {
 
-        var decoded = angular.element('<textarea />').html(html).text();
+        //var decoded = angular.element('<textarea />').html(html).text();
+
+        var decoded = SnuOwnd.getParser().render(html);
+
 
         return decoded;
     };
