e10b3390e29a0f44631a5c20cca429042ece4289
dariel312 <darielbarroso1998@gmail.com>
2018-08-15 15:33:46 -0400

    Added placeholder posts Subreddits will now scroll to top upon being changed Added user profile being displayed in the navbar Added dropdown for subscribed subreddits

--- ReReddit/wwwroot/js/app.js
+++ ReReddit/wwwroot/js/app.js
@@ -553,25 +553,31 @@ const SubredditComponent = {
         $ctrl.name = $stateParams.name;
         $ctrl.listing = [];
         $ctrl.posts = [];
+        $ctrl.placeholders = [{}, {}, {}, {}, {}];
         $ctrl.about = null;
         $ctrl.rules = null;
 
 
         $ctrl.next = function () {
             console.log($ctrl.listing);
-            api.getSubredditPosts($stateParams.name, { after: $ctrl.listing.after, count: $ctrl.posts.length }).then(function (result) {
-                $ctrl.listing = result.data.data;
-                result.data.data.children.forEach(function (item) {
-                    $ctrl.posts.push(item);
+            api.getSubredditPosts($stateParams.name, { before: $ctrl.listing.after, count: $ctrl.posts.length })
+                .then(function (result)  {
+                    $ctrl.listing = result.data.data;
+                    result.data.data.children.forEach(function (item) {
+                        $ctrl.posts.push(item);
+                    });
                 });
-            });
         }
 
         $ctrl.$onInit = function () {
+            //reset scroll
+            angular.element(window).scrollTop(0);
 
+            //download data
             api.getSubredditPosts($stateParams.name).then(function (result) {
                 $ctrl.listing = result.data.data;
                 $ctrl.posts = result.data.data.children;
+                $ctrl.placeholders = [];
             });
 
             api.getSubredditAbout($stateParams.name).then(function (result) {
